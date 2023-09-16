db2a1f16ac8be08f5b5878978b19e15191941d70
Hexcz <75824419+g3r0m4@users.noreply.github.com>
2023-09-16 19:49:34 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+if (/Mobi|Android/i.test(navigator.userAgent)) {
+ 
+  window.location.href = "g3r0m4.github.io";
+} else {
+  
+  Example: window.location.href = "github.com";
+}
+
+
+
+
 $(document).ready(() => {
   $(() => {
     $('[data-toggle="tooltip"]').tooltip();
