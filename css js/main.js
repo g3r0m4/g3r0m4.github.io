6c49aa9dd22b56b064d3f9692a339a845eab0607
g3r0m4 <g3r0m4@jeromeabanda79@gmail.com>
2023-12-28 19:49:44 +0800

    test

---
+++ css js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Your JavaScript code here
+    var button = document.getElementById('c-butt');
+    button.addEventListener('click', function() {
+      alert('message sent');
+    });
+  });
