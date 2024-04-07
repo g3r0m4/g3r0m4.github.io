879738902380366cf80d40c2ff96d064e899fb0f
g3r0m4 <g3r0m4@jeromeabanda79@gmail.com>
2024-04-07 17:36:57 +0800

    deletion

--- css js/main.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Your JavaScript code here
-    var button = document.getElementById('c-butt');
-    button.addEventListener('click', function() {
-      alert('message sent');
-    });
-  });
